193b7c6cdba8b1f9fbc030ee31450db59de288b0
Ajit Bhagwat <ajitbhagwat@Ajit-Bhagwats-MacBook-Pro.local>
2014-07-09 21:45:23 +0530

    Added javascript to switch between problems

--- home.js
+++ home.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-	$('#months').accordion();
+                  var month_number = document.URL.match(/[0-9]+(?=.)/);
+                  var panel_number = (month_number==null) ? 0 : panel_number = $('#months h3').length-parseInt(month_number);
+                  
+                  $('#months').accordion({active:panel_number});
 });
 
 $('.problemlink').click();
--- index.php
+++ index.php
@@ -11,19 +11,22 @@
 	<body>
 		<div id="wrapper">
 			<div id="months">
-				<?php 
-				/* MONTHS = LIST OF ALL MONTH FOLDERS
-				 * WEEKS = A VARIABLE LIST OF 4 PROBLEMS IN MONTH i 
+				<?php
+				/* IN THIS SECTION, WE MAKE THE NAVIGATION BAR
+                 * MONTHS = LIST OF ALL MONTH FOLDERS
+				 * WEEKS = A VARIABLE LIST OF 4 PROBLEMS ANY IN MONTH i 
 				 * FOURSET = A STRING WHICH CONCATENATES THE NAMES OF PROBLEMS 
 				 * PROBLEMNUMBER = NUMBER OF PROBLEM IN THE FORMAT 2.3, 4.1, ETC*/
 					$months = scandir("problems");
                     natsort($months); // NATURAL SORT ON THE MONTHS
+                    if(in_array('.DS_Store', $months)) unset($months[2]); // REMOVING THE CRAZY MACINTOSH FILE
                     $months = array_values($months); // REARRANGING ARRAY KEYS
-					for($i=sizeof($months)-1; $i>=2; $i--){ 
+                    $months = array_slice($months, 2);
+					for($i=sizeof($months)-1; $i>=0; $i--){
 						$weeks = scandir("problems/".$months[$i]);
-						$fourset = "<h3>".substr($months[$i], 2)."</h3><div name=".($i-1).">";
-						for($j=2; $j<sizeof($weeks); $j++){ 
-							$problemnumber = ($i-1).".".($j-1);
+						$fourset = "<h3>".substr($months[$i], 2)."</h3><div name=".($i+1).">";
+						for($j=2; $j<sizeof($weeks); $j++){
+							$problemnumber = ($i+1).".".($j-1); // MONTHS IS SLICED, WEEKS ISN'T
 							$fourset = $fourset."<p><a href=index.php?problem=".$problemnumber.
 							" class=problemlink name=".$problemnumber.">".substr($weeks[$j], 7)."</a></p>";				
 						}
@@ -46,16 +49,15 @@
 				if(!array_key_exists('problem', $_GET))
 					$problem_week = sizeof(scandir("problems/".$months[sizeof($months)-1]))-2; // -2 TO REMOVE THE FILES . AND ..
 				else
-					$problem_week = $matches[0][1];
+					$problem_week = $matches[0][1]; // WEEK NUMBER
 				
-				echo $problem_month;
-				echo ".".$problem_week;
-				
-				
-				$weeks = scandir("problems/".$months[sizeof($months)-1]);
+                $weeks = scandir("problems/".$months[$problem_month-1]);
+                natsort($weeks); // NEVER FORGET TO USE natural sort. OTHERWISE THE ARRAY ORDER WILL GO HAYWIRE.
+                $weeks = array_values($weeks); //REARRANGING ARRAY KEYS WILL GET NATSORT WORKING
+                $weeks = array_slice($weeks, 2); // GETTING RID OF . and ..
 				?>
-				<h2><?php echo(substr($months[sizeof($months)-1], 2).", Week ".substr($weeks[sizeof($weeks)-1], 5, 1)); ?></h2>
-				<h3><?php echo(substr($weeks[sizeof($weeks)-1], 7))?></h3>
+				<h2><?php echo(substr($months[$problem_month-1], 2).", Week ".substr($weeks[$problem_week-1], 5, 1)); ?></h2>
+				<h3><?php echo(substr($weeks[$problem_week-1], 7))?></h3>
 			</section>
 		</div>
 	</body>
